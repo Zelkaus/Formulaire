187ec9544f3d896b73f7050afbae697888320145
Zelkaus <82809632+Zelkaus@users.noreply.github.com>
2022-04-13 18:54:40 +0200

    Rajoute de la fonction Hash

--- Inscription.php
+++ Inscription.php
@@ -4,10 +4,7 @@
 if (isset($_POST['submit'])){
     $userEmail = $_POST['Email'];
     $userPassword = $_POST['Password'];
-
-    password_hash($userPassword, PASSWORD_DEFAULT);
-
-
+    
     $user = 'zel';
     $passwordBdd = '0000';
     $Bdd = new PDO("mysql:host=localhost;dbname=formulaire", $user, $passwordBdd);
@@ -17,7 +14,7 @@ if (isset($_POST['submit'])){
 
     $insertUser->execute([
         'Email' => $userEmail,
-        'Password' => $userPassword,
+        'Password' => password_hash($userPassword, PASSWORD_DEFAULT),
     ]);
 
     header("Location: Connexion.html");
